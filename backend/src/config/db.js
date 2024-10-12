e40b0ad88848cdc4bda6440d85bd7fc172568a35
himansu naik <naikhimansu546@gmail.com>
2024-10-12 05:43:43 +0530

    testing env and change dir outside of src and env add in db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -11,11 +11,13 @@ const knex = require("knex");
 // 	},
 // });
 
+require("dotenv").config();
+
 const db = knex({
 	client: "pg",
 	connection: {
 		connectionString: process.env.DATABASE_URL,
-		ssl: { rejectUnauthorized: false },
+		ssl: { rejectUnauthorized: true },
 	},
 });
 
