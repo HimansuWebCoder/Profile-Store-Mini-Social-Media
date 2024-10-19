285c2a90149144ce9c82d01fd25a96cc467d5ac5
himansu naik <naikhimansu546@gmail.com>
2024-10-19 12:46:20 +0530

    server static file uploads

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,6 +28,8 @@ app.use(express.static(path.join(__dirname, "./public")));
 // Import Routes
 const apiRouter = require("./routes/api/api.router");
 
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
 app.post("/upload", upload.single("avatar"), function (req, res, next) {
 	console.log("uploaded file: ", req.file);
 	console.log("uploaded file: ", req.body);
