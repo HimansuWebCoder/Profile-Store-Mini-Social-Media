d4baf4014983b7cc21d6b4db44e7afe0a2cf024d
himansu naik <naikhimansu546@gmail.com>
2024-10-20 08:25:33 +0530

    updated new form data format and frontend styles some

--- backend/src/routes/profile/profile-info/profileInfo.controller.js
+++ backend/src/routes/profile/profile-info/profileInfo.controller.js
@@ -34,7 +34,6 @@ function getProfileInfo(req, res) {
 		.then((profileInfoData) => {
 			// or > 0
 			if (profileInfoData.length !== 0) {
-				console.log("my profile info", profileInfoData[0].name);
 				return res.status(200).json(profileInfoData);
 			} else {
 				return res.status(404).json({
--- frontend/src/components/upload/Upload.js
+++ frontend/src/components/upload/Upload.js
@@ -1,21 +1,86 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
+// import "./Upload.css";
+// function Upload() {
+// 	return (
+// 		<div className="upload-container">
+// 			<div className="upload-sub-container">
+// 				<form
+// 					action="/upload"
+// 					method="post"
+// 					enctype="multipart/form-data"
+// 				>
+// 					<input type="file" name="avatar" />
+// 					<button type="submit">Upload</button>
+// 					<button>
+// 						<Link to="/admin">Back</Link>
+// 					</button>
+// 				</form>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export default Upload;
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PopupEdit from "../Popup-edit/PopupEdit";
 import "./Upload.css";
+
 function Upload() {
+	const [selectedFile, setSelectedFile] = useState(null);
+	const [popupMessage, setPopupMessage] = useState(null);
+
+	const handleFileChange = (e) => {
+		setSelectedFile(e.target.files[0]);
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (!selectedFile) {
+			alert("Please select a file to upload");
+			return;
+		}
+
+		const formData = new FormData();
+		formData.append("avatar", selectedFile);
+
+		try {
+			const response = await fetch("http://localhost:8000/upload", {
+				method: "POST",
+				body: formData,
+			});
+
+			if (response.ok) {
+				const data = await response.json();
+				console.log("File uploaded successfully:", data);
+				setPopupMessage("Upload Profile Image successfully");
+				// Redirect or handle success as needed
+			} else {
+				console.error("Failed to upload file");
+			}
+		} catch (error) {
+			console.error("Error during file upload:", error);
+		}
+	};
+
 	return (
 		<div className="upload-container">
 			<div className="upload-sub-container">
-				<form
-					action="/upload"
-					method="post"
-					enctype="multipart/form-data"
-				>
-					<input type="file" name="avatar" />
+				<form onSubmit={handleSubmit}>
+					<input
+						type="file"
+						name="avatar"
+						onChange={handleFileChange}
+					/>
 					<button type="submit">Upload</button>
 					<button>
 						<Link to="/admin">Back</Link>
 					</button>
 				</form>
+				{popupMessage && <PopupEdit msg={popupMessage} />}
 			</div>
 		</div>
 	);
