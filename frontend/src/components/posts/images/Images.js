0094cb86242b25e6c0e7327c838dad56dfe57e27
himansu naik <naikhimansu546@gmail.com>
2024-10-23 07:40:54 +0530

    add social post box and update frontend

--- frontend/src/components/posts/images/Images.js
+++ frontend/src/components/posts/images/Images.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { apiUrl } from "../../../utils/utils";
+import ProfilePhoto from "../../Profile-photo/ProfilePhoto";
+import "./Images.css";
 
 function Images() {
 	const [postImages, setPostImages] = useState([]);
@@ -14,22 +16,57 @@ function Images() {
 			});
 	}, [location]);
 	return (
-		<div>
-			<h1>Images</h1>
+		<div className="post-container">
 			{postImages.map((img) => (
-				<div
-					key={img.id}
-					style={{
-						maxWidth: "200px",
-						height: "100px",
-						border: "1px solid black",
-					}}
-				>
-					<img
-						src={img.image_url}
-						style={{ maxWidth: "50px", height: "50px" }}
-						alt="posted image"
-					/>
+				<div className="post-sub-container" key={img.id}>
+					<div className="posted-image-container">
+						<div className="post-logo-edit-container">
+							<div>
+								<ProfilePhoto
+									imgSrc="/assets/images/user.png"
+									className="posted-image-logo"
+								/>
+							</div>
+							<div>
+								<img
+									className="posted-image-logo"
+									src="/assets/images/menu.png"
+									alt="triple dot"
+								/>
+							</div>
+						</div>
+						<img
+							id="posted-img"
+							src={img.image_url}
+							alt="posted image"
+						/>
+						<div className="like-comment-share-container">
+							<div>
+								<img
+									className="posted-image-emojis"
+									src="/assets/images/like.png"
+									alt="like"
+								/>
+								<h4>Like</h4>
+							</div>
+							<div>
+								<img
+									className="posted-image-emojis"
+									src="/assets/images/comment.png"
+									alt="comment"
+								/>
+								<h4>Comment</h4>
+							</div>
+							<div>
+								<img
+									className="posted-image-emojis"
+									src="/assets/images/share.png"
+									alt="share"
+								/>
+								<h4>Share</h4>
+							</div>
+						</div>
+					</div>
 				</div>
 			))}
 		</div>
--- frontend/src/components/upload/Upload.js
+++ frontend/src/components/upload/Upload.js
@@ -1,28 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// import "./Upload.css";
-// function Upload() {
-// 	return (
-// 		<div className="upload-container">
-// 			<div className="upload-sub-container">
-// 				<form
-// 					action="/upload"
-// 					method="post"
-// 					enctype="multipart/form-data"
-// 				>
-// 					<input type="file" name="avatar" />
-// 					<button type="submit">Upload</button>
-// 					<button>
-// 						<Link to="/admin">Back</Link>
-// 					</button>
-// 				</form>
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-// export default Upload;
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import PopupEdit from "../Popup-edit/PopupEdit";
