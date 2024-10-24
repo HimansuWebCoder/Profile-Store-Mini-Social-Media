387f1a7ac6588bdbe13b7b9b5220fdf1bfe166e8
himansu naik <naikhimansu546@gmail.com>
2024-10-24 19:13:20 +0530

    updated profile photo to show everywhere where needed

--- frontend/src/components/Profile-photo/ProfilePhoto.js
+++ frontend/src/components/Profile-photo/ProfilePhoto.js
@@ -1,7 +1,23 @@
+import { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { apiUrl } from "../../utils/utils";
+import { ThemeContext } from "../../ThemeContext";
 import "./ProfilePhoto.css";
 
 function ProfilePhoto({ imgSrc, alt, size, bg, className }) {
+	const [profileImg, setProfileImg] = useState("");
+	const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		fetch(`${apiUrl}/api/profile-photo`)
+			.then((res) => res.json())
+			.then((photo) => {
+				setTimeout(() => {
+					setProfileImg(photo[photo.length - 1].image);
+					setLoading(false);
+				}, 2000);
+			});
+	}, []);
 	return (
 		<div
 			className="profile-photo-main-container"
@@ -9,12 +25,18 @@ function ProfilePhoto({ imgSrc, alt, size, bg, className }) {
 		>
 			<div className="profile-photo-container">
 				<Link id="admin-link-profile" to="/admin">
-					<img
-						className={`${className}`}
-						src={imgSrc}
-						alt={alt}
-						// style={{ width: size, height: size }}
-					/>
+					{loading ? (
+						<p style={{ color: isDarkMode ? "white" : "white" }}>
+							Loading...
+						</p>
+					) : (
+						<img
+							className={`${className}`}
+							src={profileImg}
+							alt={alt}
+							// style={{ width: size, height: size }}
+						/>
+					)}
 				</Link>
 			</div>
 		</div>
