fac65ba78e8ba6e8f60b1f981719f99f5925feb6
himansu naik <naikhimansu546@gmail.com>
2024-10-25 14:59:51 +0530

    add cloudinary for file upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,17 @@ const session = require("express-session");
 const path = require("path");
 const bodyParser = require("body-parser");
 const upload = require("./config/config");
+const cloudinary = require("cloudinary").v2;
 const db = require("./config/db");
 
 require("dotenv").config();
+
+// Configuration
+cloudinary.config({
+	cloud_name: "dtiasevyl",
+	api_key: "411418114532979",
+	api_secret: "Y4GRLW3VVy2_RwrO9TV5YMfHKFI", // Click 'View API Keys' above to copy your API secret
+});
 
 // Import Middlewares
 const bodyParserMiddleware = require("./middlewares/bodyParserMiddleware");
@@ -32,11 +40,13 @@ const apiRouter = require("./routes/api/api.router");
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.put("/upload/:id", upload.single("avatar"), function (req, res, next) {
+	const id = req.params.id;
 	console.log("uploaded file: ", req.file);
 	console.log("uploaded file: ", req.body);
 
-	const fullImgUrl = `https://profile-store-mini-social-media.onrender.com/uploads/${req.file.filename}`;
+	// const fullImgUrl = `https://profile-store-mini-social-media.onrender.com/uploads/${req.file.filename}`;
 	// const fullImgUrl = `http://localhost:8000/uploads/${req.file.filename}`;
+	// const fullImgUrl = req.file.filename;
 
 	// db.insert({ image: fullImgUrl })
 	// 	.into("profile_photo")
@@ -49,26 +59,46 @@ app.put("/upload/:id", upload.single("avatar"), function (req, res, next) {
 	// 		});
 	// 	});
 
-	const { id } = req.params;
+	const imagePath = req.file.path;
 
-	db("profile_photo")
-		.where({ id })
-		.update({ image: fullImgUrl })
-		.returning("*")
-		.then((data) => {
-			if (data.length > 0) {
-				// or data.length !== 0 must not be zero either > 0 or !== 0
-				console.log(data);
-				return res.status(200).json({
-					message: "Profile photo updated successfully",
-					data: data,
+	const uploadImage = async (imagePath) => {
+		const options = {
+			use_filename: true,
+			unique_filename: false,
+			overwrite: true,
+		};
+
+		try {
+			// Upload the image
+			const result = await cloudinary.uploader.upload(imagePath, options);
+			console.log(result.url);
+			db("profile_photo")
+				.where({ id })
+				.update({ image: result.url })
+				.returning("*")
+				.then((data) => {
+					if (data.length > 0) {
+						// or data.length !== 0 must not be zero either > 0 or !== 0
+						console.log(data);
+						return res.status(200).json({
+							message: "Profile photo updated successfully",
+							data: data,
+						});
+					} else {
+						return res.status(404).json({
+							Error: "Profile photo not found to update",
+						});
+					}
 				});
-			} else {
-				return res.status(404).json({
-					Error: "Profile photo not found to update",
-				});
-			}
-		});
+			return result.public_id;
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	// console.log(uploadImage(imagePath));
+
+	uploadImage(imagePath);
 });
 
 // API Routers
