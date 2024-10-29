86388ef35f25444b2ca0766f296bfd62a31cf988
himansu naik <naikhimansu546@gmail.com>
2024-10-29 08:35:52 +0530

    add api context and remove box shadow from profile card

---
+++ frontend/src/ApiContext.js
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { apiUrl } from "./utils/utils";
+const ApiContext = createContext();
+
+function ApiContextProvider({ children }) {
+	const [profilePhoto, setProfilePhoto] = useState("");
+	const [loading, setLoading] = useState(true);
+	const location = useLocation();
+
+	useEffect(() => {
+		setLoading(true);
+		fetch(`${apiUrl}/api/profile-photo`)
+			.then((res) => res.json())
+			.then((photo) => {
+				setTimeout(() => {
+					setProfilePhoto(photo[photo.length - 1].image);
+					setLoading(false);
+				}, 2000);
+			});
+	}, [location]);
+
+	return (
+		<ProfilePhotoContext.Provider value={{ profilePhoto, loading }}>
+			{children}
+		</ProfilePhotoContext.Provider>
+	);
+}
+
+export { ApiContext, ApiContextProvider };
