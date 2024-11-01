bd1e2450fbee06caf9e710aeb02101a061f9d91f
himansu naik <naikhimansu546@gmail.com>
2024-11-01 17:24:31 +0530

    add toggle setting

--- frontend/src/components/Setting/Setting.js
+++ frontend/src/components/Setting/Setting.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import Mode from "../Mode/Mode";
 import "./Setting.css";
 
 function ProfileSetting() {
+	const [showSettingBox, setshowSettingBox] = useState(false);
+
+	function settingBoxOpen() {
+		setshowSettingBox((prev) => !prev);
+	}
+
 	return (
 		<div className="setting-container">
-			<img src="/assets/images/setting.png" />
+			<img onClick={settingBoxOpen} src="/assets/images/setting.png" />
+			{showSettingBox && (
+				<div className="toggle-setting-box">
+					<Mode /> {/* Assuming Mode is a defined component */}
+					<h3>Feedback</h3>
+					<h3>Logout</h3>
+				</div>
+			)}
 		</div>
 	);
 }
