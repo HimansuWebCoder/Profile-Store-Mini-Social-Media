17438d324a54fb171a34c6f815d8e6da842791a9
himansu naik <naikhimansu546@gmail.com>
2024-11-11 22:10:06 +0530

    test match origin

--- backend/src/middlewares/corsMiddleware.js
+++ backend/src/middlewares/corsMiddleware.js
@@ -9,6 +9,6 @@ require("dotenv").config();
 
 // for production only
 module.exports = cors({
-	origin: "https://profile-store-mini-social-media.onrender.com/",
+	origin: "https://profile-store-mini-social-media.onrender.com",
     credentials: true
 });
