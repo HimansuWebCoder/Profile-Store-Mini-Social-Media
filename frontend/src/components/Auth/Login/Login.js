5ae4c49662c1314fcace95d08e03cd60fbc89e0b
himansu naik <naikhimansu546@gmail.com>
2024-11-12 08:13:43 +0530

    updated

--- frontend/src/components/Auth/Login/Login.js
+++ frontend/src/components/Auth/Login/Login.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { apiUrl } from "../../../utils/utils";
 import "./Login.css";
 
 function Login() {
 	const [ loginInput, setLoginInput ] = useState("");
 	const [input, setInput] = useState("");
-
+    const navigate = useNavigate();
 
 function submitHandler() {
 		fetch(`${apiUrl}/login`, {
@@ -14,6 +15,16 @@ function submitHandler() {
 				body: JSON.stringify({email: loginInput}),
 				credentials: "include"
 		})
+		.then(() => {
+           if (response.ok) {
+		        navigate("/admin");
+		      } else {
+		        console.error("Login failed");
+		      }
+		})
+		 .catch((error) => {
+	      console.error("An error occurred:", error);
+	    });
 }
 
 	function inputHandler(e) {
@@ -22,8 +33,10 @@ function submitHandler() {
 
 	return (
 		<div className="login-container">
-			<h1>I am Login page</h1>
-            <input type="text" value={loginInput} onChange={inputHandler} />
+			<h1>login testing phase now please use existing email. Use any to see result</h1>
+			<h3 style={{color: "white"}}>himansu@gmail.com</h3>
+			<h3 style={{color: "white"}}>h@gmail.com</h3>
+            <input type="text" value={loginInput} onChange={inputHandler} placeholder="himansu@gmail.com" />
             <button onClick={submitHandler}>submit</button>
 		</div>
 	);
