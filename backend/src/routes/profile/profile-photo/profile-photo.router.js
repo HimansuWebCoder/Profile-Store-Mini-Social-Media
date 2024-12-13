1244c45b20c3ed50aec0759732e40452cb757501
himansu naik <naikhimansu546@gmail.com>
2024-12-13 15:42:10 +0530

    fix issue of profile photo ensureauthenticated

--- backend/src/routes/profile/profile-photo/profile-photo.router.js
+++ backend/src/routes/profile/profile-photo/profile-photo.router.js
@@ -6,15 +6,8 @@ const {
 } = require("./profile-photo.controller");
 const profilePhotoRouter = express.Router();
 
-const ensureAuthenticated = (req, res, next) => {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect('/');
-};
-
 // GET profile-photo
-profilePhotoRouter.get("/", ensureAuthenticated, (req, res) => {
+profilePhotoRouter.get("/",(req, res) => {
 	getProfilePhoto(req, res, db);
 });
 
